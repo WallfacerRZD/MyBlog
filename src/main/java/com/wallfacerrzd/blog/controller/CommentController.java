68ab025ba7ee5427458639ee0fc3d6eca101a029
WallfacerRZD <1048058400@qq.com>
2018-05-28 17:04:59 +0800

    修改评论API, 用于后续进行增强

--- src/main/java/com/wallfacerrzd/blog/controller/CommentController.java
+++ src/main/java/com/wallfacerrzd/blog/controller/CommentController.java
@@ -5,6 +5,7 @@ import com.wallfacerrzd.blog.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.sql.Date;
 import java.util.List;
 
@@ -27,9 +28,16 @@ public class CommentController {
     }
 
     @PostMapping("/comment")
-    public void insertComments(@RequestParam("articleId") int id,
-                                  @RequestParam("content") String content,
-                                  @RequestParam("user") String user) {
-        commentService.insertAnonymousComment(id, content, new Date(System.currentTimeMillis()));
+    public void insertComments(HttpServletRequest request) {
+        try {
+            int id = Integer.parseInt(request.getParameter("articleId"));
+            String content = request.getParameter("content");
+            String user = request.getParameter("user");
+            // user is always "anonymousUser"
+            commentService.insertAnonymousComment(id, content, new Date(System.currentTimeMillis()));
+        } catch (NumberFormatException e) {
+            // if the id is invalid, do nothing
+        }
+
     }
 }
