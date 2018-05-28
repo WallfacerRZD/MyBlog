329fc8b7ac41c6abd2bfc6dd0a9921677edf96c9
WallfacerRZD <1048058400@qq.com>
2018-05-28 17:21:47 +0800

    EnableAspectJAutoProxy

---
+++ src/main/java/com/wallfacerrzd/blog/configs/AppConfig.java
@@ -0,0 +1,13 @@
+package com.wallfacerrzd.blog.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @author WallfacerRZD
+ * @date 2018/5/28 16:29
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class AppConfig {
+}
